6547cd96ad709aa0ce6b93a82a0d5db8d9299abd
Leonardo Silveira <sombriks@gmail.com>
2021-12-15 02:40:55 -0300

    Update validator.test.ts

--- src/components/validator.test.ts
+++ src/components/validator.test.ts
@@ -1,13 +1,19 @@
+// src/components/validator.test.ts
 import { validateNumber } from "./validator";
+
 describe("validator test suite", () => {
+
   it("should validate number divisible by 3", () => {
     const fn = jest.fn(validateNumber);
 
     const number1: number = 5;
     const number2: number = 9;
 
-    expect(fn(number1)).toBe(false);
-    expect(fn(number2)).toBe(true);
+    const result1: boolean = fn(number1);
+    const result2: boolean = fn(number2); 
+
+    expect(result1).toBe(false);
+    expect(result2).toBe(true);
     expect(fn).toBeCalled();
   });
 });
